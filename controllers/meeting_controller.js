b60be4d94c2b7211ce3083b3de5ff225cf3f36c2
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-08-23 02:59:39 +0530

    authenticate room request

--- controllers/meeting_controller.js
+++ controllers/meeting_controller.js
@@ -1,6 +1,7 @@
 const { v4: uuidV4 } = require('uuid')
 
 module.exports.createRoomId = function(req,res){
+    console.log("Broooooo")
     if(req.isAuthenticated){
         res.redirect(`/meet/room/${uuidV4()}`);
     }else{
@@ -10,8 +11,7 @@ module.exports.createRoomId = function(req,res){
 
 module.exports.enterRoom = function(req,res){
     if(req.isAuthenticated){
+        console.log("Here");
         return res.render('editor/editor.ejs',{roomId : req.params.roomId});
-    }else{
-        return res.redirect('/users-sign-in');
     }
 }
--- routes/new_meeting.js
+++ routes/new_meeting.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const roomController = require('../controllers/meeting_controller');
 
 router.get('/start-instant-meet',roomController.createRoomId);
-router.get('/room/:roomId',roomController.enterRoom);
+router.get('/room/:roomId',passport.checkAuthentication,roomController.enterRoom);
 
 
 module.exports = router;

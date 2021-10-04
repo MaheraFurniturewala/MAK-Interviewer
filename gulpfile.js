ad517ee0772e12c2b7d06efb2dadb10e0aa0ec1b
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-10-04 20:19:32 +0530

    compress assets and create manifest

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,71 @@
 const gulp = require('gulp');
+
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
+//-----gulp tasks--------
+
+gulp.task('css',function(done){
+    console.log('minifying cs...');
+    gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass()) //sass to css
+    .pipe(cssnano()) //minify css
+    .pipe(gulp.dest('./assets.css'));
+
+    //changing naming convention
+     gulp.src('./assets/**/*.css')
+    .pipe(rev()) //renaming
+    .pipe(gulp.dest('./public/assets')) //for production
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+
+gulp.task('js', function(done){
+    console.log('minifying js...');
+     gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done()
+});
+
+
+gulp.task('images', function(done){
+    console.log('compressing images...');
+    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+
+// empty the public/assets directory
+gulp.task('clean:assets', function(done){
+    del.sync('./public/assets');
+    done();
+});
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+    console.log('Building assets');
+    done();
+});

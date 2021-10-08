930a268f1409df2becaad4519255972e47915f34
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-10-08 16:22:49 +0530

    remove development object

--- config/environment.js
+++ config/environment.js
@@ -10,29 +10,6 @@ const accessLogStream = rfs.createStream('access.log',{
     path: logDirectory,
 });
 
-const development = {
-    name : 'development',
-    asset_path : './public/assets',
-    session_cookie_key :'xyzabc',
-    db: 'mongodb+srv://mahera1:mahera1@cluster0.k05c5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    smtp :{
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'interviewermak@gmail.com',
-            pass: 'mak_interviewer3'
-        }
-    },
-    google_client_id:'736952970227-ik81a2s746sogr6nar8i6pl9fgj1fbr0.apps.googleusercontent.com',
-    google_client_secret: '5_tegMySOYY-Q0cTzOa7Qarg',
-    google_call_back_url: "http://mak-interviewer.herokuapp.com/users/auth/google/callback",
-    morgan : {
-        mode: 'dev',
-        options: {stream: accessLogStream}
-    }
-}
 
 
 const production = {
